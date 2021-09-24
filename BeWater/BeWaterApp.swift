835a60dd753ab7af16caa3a87552b7a7dea9865d
Halil.KAYA <kayaantalya7@gmail.com>
2021-09-24 23:59:35 +0300

    SwiftUI App

---
+++ BeWater/BeWaterApp.swift
@@ -0,0 +1,16 @@
+//
+//  BeWaterApp.swift
+//  BeWater
+//
+//
+
+import SwiftUI
+
+@main
+struct BeWaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
